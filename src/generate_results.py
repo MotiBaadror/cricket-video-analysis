aa5f4121745197a2f730082d22439032177606a6
Moti Ram <motibaadror@gmail.com>
2021-05-22 14:15:23 +0530

    code cleaning

--- src/generate_results.py
+++ src/generate_results.py
@@ -2,20 +2,27 @@ import cv2
 import numpy as np
 import tqdm
 import shutil
+import os
+from getpoints import getpoints
+from overlapping_interval import overlapping_interval
 # new_out = [510, 1965, 1995, 2010, 2035, 2040, 3455, 4260, 5980]
-name = 'results/detfps60fd2normalbatplusballinterval/'
+name = '../results/detfps60fd2normalbatplusballinterval/'
+
 
 try:
     shutil.rmtree(name)
 except:
     print('does not exist')
 os.makedirs(name, exist_ok=True)
-cap = cv2.VideoCapture('../data/video.mp4')
+cap = cv2.VideoCapture('../../data/video.mp4')
  
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # out = cv2.VideoWriter('results/red_cricket.mp4',fourcc, 20, (1280,720))
 k = 0
 # frames_int = [ frames_sorted[i] for i in frames_from_int]
+num_out, bat, ball, cor_bat, cor_ball = getpoints()
+
+frame_from_int = overlapping_interval(bat,ball,cor_bat,cor_ball)
 while cap.isOpened():
     
     ret, frame = cap.read()
--- src/overlapping_interval.py
+++ src/overlapping_interval.py
@@ -1,97 +1,101 @@
 import math
-i =0 
-j =0 
-th = 1000
-ldist = th
-frames =[]
-while (i<len(ball) and j< len(bat)):
-    # print(i,j)
-    # print('checking')
-    while (i<len(ball) and j < len(bat)) and abs(ball[i]- bat[j])<50:
-       
-        # print('here i am')
-        bcx,bcy,cx,cy = cor_ball[i][0],cor_ball[i][1], cor_bat[j][0]+cor_bat[j][2]//2,\
-            cor_bat[j][1]+cor_bat[j][3]//2,
-        dist = math.sqrt(((bcx-cx)*(bcx-cx))+((bcy-cy)*(bcy-cy)))
-        print(ball[i],bat[j], dist)
-        if dist<ldist:
-            if ldist !=th:
-                print('deleted ', frames[-1])
-                del frames[-1]
-                # del frames[-1]
 
-                # del frames[-1]
-            print('saving frame ' ,ball[i])
-            frames.append(ball[i])
-            # frames.append(bat[i])
-            # frames.append(bat[j])
-            ldist = dist
-        if ball[i]>bat[j]:
-            j+=1
-            # ldist=th
-        else:
-            i+=1
-            # ldist = th
-            
-    try:
-        if ball[i]>bat[j]:
-            j+=1 
-            ldist =th
-        else:
-            i+=1
-            ldist = th 
-    except:
-        print(i, j)
-        continue 
+def overlapping_interval(bat, ball,cor_bat, cor_ball):
+    i =0 
+    j =0 
+    th = 1000
+    ldist = th
+    frames =[]
+    while (i<len(ball) and j< len(bat)):
+        # print(i,j)
+        # print('checking')
+        while (i<len(ball) and j < len(bat)) and abs(ball[i]- bat[j])<50:
         
-print(frames)
+            # print('here i am')
+            bcx,bcy,cx,cy = cor_ball[i][0],cor_ball[i][1], cor_bat[j][0]+cor_bat[j][2]//2,\
+                cor_bat[j][1]+cor_bat[j][3]//2,
+            dist = math.sqrt(((bcx-cx)*(bcx-cx))+((bcy-cy)*(bcy-cy)))
+            print(ball[i],bat[j], dist)
+            if dist<ldist:
+                if ldist !=th:
+                    print('deleted ', frames[-1])
+                    del frames[-1]
+                    # del frames[-1]
 
-
-
-
-import math
-i =len(ball)-1 
-j =len(bat)-1
-th = 1000
-ldist = th
-frames_rev =[]
-while (i>=0 and j>=0):
-    # print(i,j)
-    # print('checking')
-    while (i>=0 and j>=0) and abs(ball[i]- bat[j])<150:
-       
-        # print('here i am')
-        bcx,bcy,cx,cy = cor_ball[i][0],cor_ball[i][1], cor_bat[j][0]+cor_bat[j][2]//2,\
-            cor_bat[j][1]+cor_bat[j][3]//2,
-        dist = math.sqrt(((bcx-cx)*(bcx-cx))+((bcy-cy)*(bcy-cy)))
-        print(ball[i],bat[j], dist)
-        if dist<ldist:
-            if ldist !=th:
-                print('deleted ', frames[-1])
-                del frames_rev[-1]
-                del frames_rev[-1]
-
-                # del frames[-1]
-            print('saving frame ' ,ball[i])
-            frames_rev.append(ball[i])
-            frames_rev.append(bat[i])
-            # frames.append(bat[j])
-            ldist = dist
-        if ball[i]>bat[j]:
-            i-=1
-            # ldist=th
-        else:
-            j-=1
-            # ldist = th
+                    # del frames[-1]
+                print('saving frame ' ,ball[i])
+                frames.append(ball[i])
+                # frames.append(bat[i])
+                # frames.append(bat[j])
+                ldist = dist
+            if ball[i]>bat[j]:
+                j+=1
+                # ldist=th
+            else:
+                i+=1
+                # ldist = th
+                
+        try:
+            if ball[i]>bat[j]:
+                j+=1 
+                ldist =th
+            else:
+                i+=1
+                ldist = th 
+        except:
+            print(i, j)
+            continue 
             
-    try:
-        if ball[i]>bat[j]:
-            i-=1 
-            ldist =th
-        else:
-            j-=1
-            ldist = th 
-    except:
-        print(i, j)
-        continue 
-print(frames_rev)
+    print(frames)
+
+
+
+
+    # import math
+    i =len(ball)-1 
+    j =len(bat)-1
+    th = 1000
+    ldist = th
+    frames_rev =[]
+    while (i>=0 and j>=0):
+        # print(i,j)
+        # print('checking')
+        while (i>=0 and j>=0) and abs(ball[i]- bat[j])<150:
+        
+            # print('here i am')
+            bcx,bcy,cx,cy = cor_ball[i][0],cor_ball[i][1], cor_bat[j][0]+cor_bat[j][2]//2,\
+                cor_bat[j][1]+cor_bat[j][3]//2,
+            dist = math.sqrt(((bcx-cx)*(bcx-cx))+((bcy-cy)*(bcy-cy)))
+            print(ball[i],bat[j], dist)
+            if dist<ldist:
+                if ldist !=th:
+                    print('deleted ', frames[-1])
+                    del frames_rev[-1]
+                    del frames_rev[-1]
+
+                    # del frames[-1]
+                print('saving frame ' ,ball[i])
+                frames_rev.append(ball[i])
+                frames_rev.append(bat[i])
+                # frames.append(bat[j])
+                ldist = dist
+            if ball[i]>bat[j]:
+                i-=1
+                # ldist=th
+            else:
+                j-=1
+                # ldist = th
+                
+        try:
+            if ball[i]>bat[j]:
+                i-=1 
+                ldist =th
+            else:
+                j-=1
+                ldist = th 
+        except:
+            print(i, j)
+            continue 
+    print(frames_rev)
+    final_frame = frames_rev+ frames
+    return final_frame
